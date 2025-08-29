e17464b1a1de1a5baffbc303bf1965c2a96267b0
rohiths0402 <113751332+rohiths0402@users.noreply.github.com>
2025-08-29 11:37:04 +0530

    Update containsDuplicate.c

--- containsDuplicate.c
+++ containsDuplicate.c
@@ -1,13 +1,27 @@
-int cmpfunc(const void *a, const void *b){
-    return (*(int*)a - *(int*)b);
+#define TABLE_SIZE 10000
+
+typedef struct Node{
+    int val;
+    struct Node* next;
+}Node;
+
+int hash(int x){
+    return (x % TABLE_SIZE + TABLE_SIZE) % TABLE_SIZE;
 }
 
 bool containsDuplicate(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), cmpfunc);
-    for(int i = 1;i < numsSize;i++){
-        if(nums[i] == nums[i - 1]){
-            return true;
+   Node* table[TABLE_SIZE] = {0};
+    for(int i = 0; i < numsSize; i++){
+        unsigned int idx = hash(nums[i]);
+        Node* cur = table[idx];
+        while(cur){
+            if (cur->val == nums[i]) return true;
+            cur = cur->next;
         }
+        Node* newNode = (Node*)malloc(sizeof(Node));
+        newNode->val = nums[i];
+        newNode->next = table[idx];
+        table[idx] = newNode;
     }
     return false;
 }
